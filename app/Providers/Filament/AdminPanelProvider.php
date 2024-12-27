fca46c1c7bd459a911aea477c5563c587123dfc9
Lakhya <lakhya.deep4914@gmail.com>
2024-12-27 20:51:30 +0530

    view feeback

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -51,6 +51,8 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
+            ->sidebarCollapsibleOnDesktop()
+            ->profile(isSimple: false)
             ->authMiddleware([
                 Authenticate::class,
             ]);
